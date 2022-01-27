684a1d4cd52f98b1815831dbfc74b39f42ef7dbe
Anil1973-13 <96384565+Anil1973-13@users.noreply.github.com>
2022-01-27 23:09:24 +0530

    Add files via upload

---
+++ Jan26/Question1.js
@@ -0,0 +1,15 @@
+var a = prompt("Enter the value of A");
+var b = prompt("Enter the value of B");
+var c = prompt("Enter the value of C");
+
+if (a<b && a<c){
+alert("A is the smallest value of three");
+}
+
+else if(b<c){
+alert("B is the smallest value of all three");
+}
+else{
+
+alert("C is the smallest of all three");
+}
---
+++ Jan26/Question2.js
@@ -0,0 +1,16 @@
+var ch = prompt("Enter a Character");
+ch = ch.toUpperCase();
+
+if(ch== "A-Z"){
+if(ch == 'A' || ch =='E' || ch =='I' || ch =='O' || ch =='U'){
+alert("Ch is Vowel");
+}
+else{
+
+alert("Ch is Consonant");
+}
+}
+else{
+
+alert("Ch is neither vowel nor consonant");
+}
