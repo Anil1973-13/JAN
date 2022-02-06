7fd53900f553a0d73ebf3bfa7384b5f904da7bde
Anil1973-13 <96384565+Anil1973-13@users.noreply.github.com>
2022-02-06 15:41:10 +0530

    Update Question2.js

--- Jan26/Question2.js
+++ Jan26/Question2.js
@@ -1,7 +1,7 @@
 var ch = prompt("Enter a Character");
 ch = ch.toUpperCase();
 
-if(ch>= "A" && ch<="Z"){
+if(ch>= 'A' && ch<='Z'){
 if(ch == 'A' || ch =='E' || ch =='I' || ch =='O' || ch =='U'){
 alert("Ch is Vowel");
 }
